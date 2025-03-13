c89affd62fa9f9b9a6c8e2affe727216abf8547d
Mustafa Aksu <mustafa@jentis.com>
2025-03-13 13:26:43 +0100

    test: intro

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nodejs-udemy-course
+
 NodeJs udemy course repo
+
+Access node cmd
+node
+
+Run a file
+
+node index.js
---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+// Read and write files syncronously --> Blocking code
+const textInput = fs.readFileSync("./txt/input.txt", "utf8");
+
+const textOutput = `This is info about avocado : ${textInput}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOutput);
+
+console.log(`Reading file synchronously : ${textInput}`);
+
+console.log("-------------------------------------------------------");
+
+// Read and write files asynchronously --> Non-blocking code
+
+// ! Callback Hell : Do not use many callbaacks in a row. Instead use promises or Async/Await
+fs.readFile("./txt/start.txt", "utf8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf8", (err, data3) => {
+      const textOutput = `${data2}\n${data3}`;
+      fs.writeFile("./txt/final.txt", textOutput, (err) => {
+        console.log("Your file has been written");
+      });
+    });
+  });
+});
+
+fs.readFile("./txt/star2.txt", "utf8", (err, data1) => {
+  if (err) return console.log(err);
+});
